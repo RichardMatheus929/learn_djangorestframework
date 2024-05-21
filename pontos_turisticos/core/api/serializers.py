69f80031f53b24e9fc5bafb09bb56fd693038a36
Richard Matheus <richardmatheus929@gmail.com>
2024-05-21 11:42:47 -0300

    Modificando o serializer para retornar chaves estrangeiras

--- pontos_turisticos/core/api/serializers.py
+++ pontos_turisticos/core/api/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework.serializers import ModelSerializer
 from core.models import PontoTuristico
+from atracoes.api.serializers import AtracaoSerializer
 
 class PontoTuristicoSerializer(ModelSerializer):
+    atracoes = AtracaoSerializer()
     class Meta:
         model = PontoTuristico
-        fields = ('id','nome','descricao','aprovado')
+        fields = ('id','nome','descricao','aprovado','atracoes')
--- pontos_turisticos/pontos_turisticos/settings.py
+++ pontos_turisticos/pontos_turisticos/settings.py
@@ -37,11 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework.authtoken',
     'rest_framework',
     'django_filters',
     'core',
     'atracoes',
-    'comentarios',
+    'comentarios', 
     'avaliacoes',
     'enderecos'
 ]
